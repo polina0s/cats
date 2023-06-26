ff4e8af60bab1e28899941d062182fc7ca282136
polina0s <polina.osintseva000@gmail.com>
2023-06-26 17:39:23 +0400

    add getCats method for api class

--- src/api/api.js
+++ src/api/api.js
@@ -3,4 +3,10 @@ class Api {
     this.link = import.meta.env.VITE_LINK;
     this.key = import.meta.env.VITE_KEY;
   }
+
+  async getCats() {
+    const response = await fetch(`${link}${key}`);
+    const json = await response.json();
+    return json;
+  }
 }
