c50e2506f4c52db2f9b81834235a150456a081e3
polina0s <polina.osintseva000@gmail.com>
2023-06-26 17:36:57 +0400

    add api class

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+class Api {
+  constructor() {
+    this.link = import.meta.env.VITE_LINK;
+    this.key = import.meta.env.VITE_KEY;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './styles/main.scss';
 import * as bootstrap from 'bootstrap';
 
-const link = import.meta.env.VITE_LINK;
-const key = import.meta.env.VITE_KEY;
+// const link = import.meta.env.VITE_LINK;
+// const key = import.meta.env.VITE_KEY;
 
 // const response = await fetch(`${link}${key}`);
 // const json = await response.json();
