e2522b7e7a21a78ae9eb193132628a0cb68faec6
polina0s <polina.osintseva000@gmail.com>
2023-07-05 16:54:43 +0400

    layout for big card

--- src/components/card/bigCard.js
+++ src/components/card/bigCard.js
@@ -6,3 +6,10 @@ class BigCard {
     this.element.classList.add('bigCard-container');
   }
 }
+
+//<div class="card text-center" style="width: 45rem">
+//  <img src="./pics/frog.png" class="card-img-top" alt="..." />
+//  <div class="card-body">
+//    <a href="#" class="btn btn-dark"> back </a>
+//  </div>
+//</div>
