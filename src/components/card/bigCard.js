7541755020cbfe161b52504972a5b24cfbda2379
polina0s <polina.osintseva000@gmail.com>
2023-07-10 18:34:50 +0400

    fixed layout for big card

--- src/components/card/bigCard.js
+++ src/components/card/bigCard.js
@@ -37,8 +37,8 @@ export class BigCard {
     this.element = document.createElement('div');
 
     this.element.innerHTML = `
-    <div class="card">
-      <div class="card-header"> <h5>${this.name}</h5> </div>
+    <div class="card mb-4">
+      <div class="card-header bg-info-subtle"> <b>${this.name}</b> </div>
       <div class="row g-0">
         <div class="col-md-6">
           <img src="${
@@ -48,54 +48,56 @@ export class BigCard {
         <div class="col-md-6">
           <div class="card-body">
             <h6 class="card-title">${this.description}</h5>
-            <p class="card-text">Temperament: ${this.temperament}</p>
-            <p class="card-text">Life span: ${this.lifeSpan} years</p>
-            <p class="card-text">Origin: ${this.origin}</p>
-            <a href="${this.wiki}" class="card-link">Wikipedia</a>
-            <ul class="list-group">
-              <li class="list-group-item">adaptibility
+            <p class="card-text"><b>Temperament:</b> ${this.temperament}</p>
+            <p class="card-text"><b>Life span:</b> ${this.lifeSpan} years</p>
+            <p class="card-text"><b>Origin:</b> ${this.origin}</p>
+            <a href="${
+              this.wiki
+            }" class="card-link link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"><b>Wikipedia</b></a>
+            <ul class="list-group mt-3 mb-4">
+              <li class="list-group-item"><b>adaptibility</b>
                 <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
                   <div class="progress-bar bg-info" style="width: ${
                     20 * this.adaptibility
                   }%"></div>
                 </div>
               </li>
-              <li class="list-group-item">social needs
+              <li class="list-group-item"><b>social needs</b>
                 <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
                   <div class="progress-bar bg-info" style="width: ${
                     20 * this.socialNeeds
                   }%"></div>
                 </div>
               </li>
-              <li class="list-group-item">child friendly
+              <li class="list-group-item"><b>child friendly</b>
                 <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
                   <div class="progress-bar bg-info" style="width: ${
                     20 * this.childFriendly
                   }%"></div>
                 </div>
               </li>
-              <li class="list-group-item">dog friendly
+              <li class="list-group-item"><b>dog friendly</b>
                 <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
                   <div class="progress-bar bg-info" style="width: ${
                     20 * this.dogFriendly
                   }%"></div>
                 </div>
               </li>
-              <li class="list-group-item">stranger friendly
+              <li class="list-group-item"><b>stranger friendly</b>
                 <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
                   <div class="progress-bar bg-info" style="width: ${
                     20 * this.strangerFriendly
                   }%"></div>
                 </div>
               </li>
-              <li class="list-group-item">health issues
+              <li class="list-group-item"><b>health issues</b>
                 <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
                   <div class="progress-bar bg-info" style="width: ${
                     20 * this.health
                   }%"></div>
                 </div>
               </li>
-              <li class="list-group-item">intelligence
+              <li class="list-group-item"><b>intelligence</b>
                 <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
                   <div class="progress-bar bg-info" style="width: ${
                     20 * this.intelligence
