05a10ecaab7166a6b0ef398330b0d83724f8716d
polina0s <polina.osintseva000@gmail.com>
2023-07-18 17:41:43 +0400

    fixed methods for catalog class

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -4,12 +4,14 @@ import { Loader } from '../components/loader/loader';
 import { Pagination } from '../components/pagination/pagination';
 import { Select } from '../components/select/select';
 import { BREEDS_OPTIONS, BREEDS_MAP } from '../config/breeds';
+import queryString from 'query-string';
 
 export class CatalogPage {
   constructor() {
-    const urlSearchParams = new URLSearchParams(location.search);
-    this.page = +urlSearchParams.get('page') || 1;
+    this.urlSearchParams = new URLSearchParams(location.search);
+    this.page = +this.urlSearchParams.get('page') || 1;
     this.breed = null;
+    this.hasBreeds = +this.urlSearchParams.get('breeds');
 
     this.catalogRow = document.querySelector('#catalog-row');
     this.selectContainer = document.querySelector('#select-container');
@@ -42,32 +44,42 @@ export class CatalogPage {
   }
 
   handlePageChange(newPage) {
-    history.replaceState(null, null, `?page=${newPage}`);
+    const query = queryString.stringify(
+      { page: newPage, breeds: this.breed },
+      { skipNull: true }
+    );
+
+    history.replaceState(null, null, `?${query}`);
     this.catalogRow.innerHTML = '';
     this.renderCatalog();
   }
 
   handleFilterChange(value) {
     this.breed = BREEDS_MAP.get(value);
+    const query = queryString.stringify(
+      { page: 1, breeds: this.breed },
+      { skipNull: true }
+    );
 
-    history.replaceState(null, null, `?page=1&${value}`);
+    history.replaceState(null, null, `?${query}`);
     this.catalogRow.innerHTML = '';
     this.renderCatalog();
+    this.pagination.setPage(1);
   }
 
   renderNavigation() {
-    const pagination = new Pagination({
+    this.pagination = new Pagination({
       defaultPage: this.page,
       onPageChange: (newPage) => this.handlePageChange(newPage),
     });
 
-    document.querySelector('body').append(pagination.element);
+    document.querySelector('body').append(this.pagination.element);
   }
 
   renderFilters() {
     const breedSelect = new Select({
       onChange: (e) => this.handleFilterChange(e.target.value),
-      defaultSelected: this.breed,
+      defaultSelected: this.hasBreeds,
       options: BREEDS_OPTIONS,
     });
 
